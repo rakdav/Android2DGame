1b0b6b13a5f5201d594b7de8d3a50b5f8725be28
Andrey Dubinin <rakdav@inbox.ru>
2021-03-26 19:01:24 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android2dgame/GameSurface.java
@@ -0,0 +1,28 @@
+package com.example.android2dgame;
+
+import android.content.Context;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class GameSurface extends SurfaceView implements SurfaceHolder.Callback {
+    public GameSurface(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+    }
+}
---
+++ app/src/main/java/com/example/android2dgame/GameThread.java
@@ -0,0 +1,4 @@
+package com.example.android2dgame;
+
+public class GameThread extends Thread {
+}
